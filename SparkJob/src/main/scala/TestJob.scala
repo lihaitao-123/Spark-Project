992452329540e9118a03d0ebe05f46ceaa755e05
lihaitao-123 <2250088288@qq.com>
2022-02-06 22:36:11 +0800

    refresh the pom.xml and add test Spark Job

--- SparkJob/src/main/scala/TestJob.scala
+++ SparkJob/src/main/scala/TestJob.scala
@@ -4,6 +4,7 @@ object TestJob {
   def main(args: Array[String]): Unit = {
     val session: SparkSession = SparkSession.builder().master("local[4]").getOrCreate()
     val context = session.sparkContext
-    context.
+    val value = context.makeRDD(Seq(123, 1234, 12345))
+    value.foreach(println)
   }
 }
